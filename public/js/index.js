6afba8a394e081ac43edb52914d5f891e56ff294
hshshs35 <huangsha4248@gmail.com>
2017-11-29 20:47:57 -0500

    jquery

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,22 @@ socket.on('disconnect', function(){
     console.log('disconnected from server');
 });
 
-socket.on('newMessage', (message)=>{
+socket.on('newMessage', function(message){
     console.log('newMessage', message);
+    var li = $('<li></li>');
+    li.text(message.from + ':' + message.text);
+
+    $('#messages').append(li);
+});
+
+$('#message-form').on('submit', function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function(){
+
+    });
+
 });
